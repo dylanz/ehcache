eaf36676bd92e9ebf8d6823a3bfceb86a2b51592
Jason Voegele <jason@jvoegele.com>
2010-09-15 18:22:11 -0700

    Fix cyclic include error.

--- lib/ehcache/java.rb
+++ lib/ehcache/java.rb
@@ -1,6 +1,6 @@
 module Ehcache
   module Java
-    include Java
+    include ::Java
     Dir["#{Ehcache::EHCACHE_HOME}/ext/**/*.jar"].sort.each {|l| require l}
     include_package "net.sf.ehcache"
     include_package "net.sf.ehcache.config"
