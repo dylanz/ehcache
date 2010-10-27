80094450913336736dc0ad94042108f68651b407
Jason Voegele <jason@jvoegele.com>
2010-10-27 12:55:40 -0400

    Delete status.rb

--- lib/ehcache/status.rb
+++
@@ -1,9 +0,0 @@
-module Ehcache
-  module Status
-    UNINITIALISED = Ehcache::Java::Status::STATUS_UNINITIALISED
-    ALIVE         = Ehcache::Java::Status::STATUS_ALIVE
-    SHUTDOWN      = Ehcache::Java::Status::STATUS_SHUTDOWN
-
-    STATUSES = [UNINITIALISED, ALIVE, SHUTDOWN]
-  end
-end
