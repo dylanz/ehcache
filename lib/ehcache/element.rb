2c0896256aff1b57e498e5f413df73c03f7a7eaf
Jason Voegele <jason@jvoegele.com>
2010-10-29 14:01:00 -0400

    Remove disabled code.

--- lib/ehcache/element.rb
+++ lib/ehcache/element.rb
@@ -15,32 +15,3 @@ class Java::NetSfEhcache::Element
   alias tti= setTimeToIdle
   alias ttl= setTimeToLive
 end
-
-__END__
-module Ehcache
-  class Element
-    attr_accessor :key, :value
-
-    def initialize(key, value, options = {})
-      @key     = key
-      @value   = value
-      @ttl     = options[:ttl] || nil
-
-      element = Ehcache::Java::Element.new(key, value)
-      element.set_time_to_live(@ttl) if @ttl
-      @proxy = element
-    end
-
-    def ttl
-      @ttl = @proxy.get_time_to_live
-    end
-
-    def tti
-      @tti = @proxy.get_time_to_idle
-    end
-
-    def proxy
-      @proxy
-    end
-  end
-end
