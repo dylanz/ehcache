bc6f2a9be3329b1fdc27e484d86cd6e671107262
Jason Voegele <jason@jvoegele.com>
2010-11-02 13:37:53 -0400

    Try to use Ehcache from CLASSPATH before bundled.

--- lib/ehcache/java.rb
+++ lib/ehcache/java.rb
@@ -1,6 +1,17 @@
-module Ehcache
-  Dir["#{Ehcache::EHCACHE_HOME}/ext/**/*.jar"].sort.each {|l| require l}
+require 'java'
 
+begin
+  # If Ehcache is already on the classpath, use it.
+  ehcache_version = Java::NetSfEhcacheUtil::ProductInfo.new.getVersion()
+  puts("Using Ehcache #{ehcache_version} from CLASSPATH")
+rescue NameError
+  # If not, use the Ehcache bundled with the jruby-ehcache gem.
+  Dir["#{Ehcache::EHCACHE_HOME}/ext/**/*.jar"].sort.each {|l| require l}
+  ehcache_version = Java::NetSfEhcacheUtil::ProductInfo.new.getVersion()
+  puts("Using bundled Ehcache #{ehcache_version}")
+end
+
+module Ehcache
   include_package 'net.sf.ehcache'
 
   module Config
