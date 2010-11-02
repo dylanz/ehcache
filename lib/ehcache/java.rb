78321bde6b93b04894ae1a46435f3195e6462506
Jason Voegele <jason@jvoegele.com>
2010-11-02 14:14:53 -0400

    Use CLASSPATH before bundled JARs.

--- lib/ehcache/java.rb
+++ lib/ehcache/java.rb
@@ -1,17 +1,34 @@
 require 'java'
 
-begin
-  # If Ehcache is already on the classpath, use it.
-  ehcache_version = Java::NetSfEhcacheUtil::ProductInfo.new.getVersion()
-  puts("Using Ehcache #{ehcache_version} from CLASSPATH")
-rescue NameError
-  # If not, use the Ehcache bundled with the jruby-ehcache gem.
-  Dir["#{Ehcache::EHCACHE_HOME}/ext/**/*.jar"].sort.each {|l| require l}
-  ehcache_version = Java::NetSfEhcacheUtil::ProductInfo.new.getVersion()
-  puts("Using bundled Ehcache #{ehcache_version}")
-end
+EHCACHE_LIBS_DIR = "#{Ehcache::EHCACHE_HOME}/ext"
 
 module Ehcache
+  slf4j_loader = lambda { Java::OrgSlf4j::LoggerFactory.getLogger("JRubyEhcache") }
+  begin
+    LOG = slf4j_loader.call
+    LOG.info("Using SLF4J Logger from CLASSPATH")
+  rescue NameError
+    Dir["#{EHCACHE_LIBS_DIR}/**/*slf4j*.jar"].each do |l| require l end
+    LOG = slf4j_loader.call
+    LOG.info("Using bundled SLF4J Logger")
+  end
+
+  ehcache_version_loader = lambda {
+    Java::NetSfEhcacheUtil::ProductInfo.new.getVersion()
+  }
+  begin
+    # If Ehcache is already on the classpath, use it.
+    VERSION = ehcache_version_loader.call
+    LOG.info("Using Ehcache #{VERSION} from CLASSPATH")
+  rescue NameError
+    # If not, use the Ehcache bundled with the jruby-ehcache gem.
+    Dir["#{EHCACHE_LIBS_DIR}/**/*.jar"].each do |l|
+      require l unless l =~ /slf4j/
+    end
+    VERSION = ehcache_version_loader.call
+    LOG.info("Using bundled Ehcache #{VERSION}")
+  end
+
   include_package 'net.sf.ehcache'
 
   module Config
